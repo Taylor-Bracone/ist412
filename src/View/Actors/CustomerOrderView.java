a9bd742ec7652f994fc63b9250da438bb1992d92
kawin <kawinters222@gmail.com>
2023-04-18 00:55:30 -0400

    Fixed authView

--- src/View/Actors/CustomerOrderView.java
+++ src/View/Actors/CustomerOrderView.java
@@ -8,60 +8,45 @@ import Model.Actors.User;
 import Model.Restaurant.Restaurant;
 import Model.Restaurant.RestaurantList;
 
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.util.ArrayList;
 import java.util.Scanner;
 
-public class CustomerOrderView {
-
+public class CustomerOrderView extends JFrame implements ActionListener {
     private CustomerController customerController = new CustomerController();
+    private JPanel optionsView = new JPanel();
+    private JList<String> optionsList;
+    private JButton btn_select, btn_exit;
+    private Customer customer;
 
     public CustomerOrderView(Customer customer){
-        System.out.println("Welcome to the Customer Order View");
-        System.out.println("Select A Functionality: ");
-        System.out.println("1. Place Order");
-        System.out.println("2. See Order History");
-        System.out.println("3. Update your account");
-        System.out.println("4. See your active orders");
-        System.out.println("5. Exit Application");
+        initCompts();
+        setVisible(true);
+        this.customer = customer;
+    }
 
-        Scanner scanner = new Scanner(System.in);
-        int option = scanner.nextInt();
+    public void initCompts(){
+        setTitle("Customer Options View");
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setSize(250, 250);
+        setLocationRelativeTo(null);
 
-        switch(option){
-            case 1:
-                System.out.println("Select Restaurant to Order from... ");
-                customerController.viewRestaurantList();
-                String restaurantName = orderFromRestaurant();
-                customerController.placeOrder(restaurantName, customer);
-                break;
+        String[] options = {"Place Order", "See Order History", "Update your account", "See your active orders"};
+        optionsList = new JList<>(options);
+        optionsView.add(optionsList);
 
-            case 2:
-                System.out.println("Your order history... ");
-                customerController.customerOrderHistory(customer);
-                break;
+        JPanel buttonPanel = new JPanel();
+        btn_select = new JButton("Select");
+        btn_select.addActionListener(this);
+        btn_exit = new JButton("Exit");
+        buttonPanel.add(btn_select);
+        buttonPanel.add(btn_exit);
 
-            case 3:
-                System.out.println("Updating your account... ");
-                System.out.println("Enter your first name: ");
-                String newFirstName = scanner.next();
-
-                System.out.println("Enter your last name: ");
-                String newLastName = scanner.next();
-
-                System.out.println("Enter your address: ");
-                String newAddress = scanner.next();
-
-                customerController.updateCustomerAccount(newFirstName, newLastName, newAddress);
-                break;
-
-            case 4:
-                System.out.println("Viewing your current orders...");
-                customerController.showRestaurantOrders(customer);
-                break;
-            case 5:
-                System.out.println("Thank you!");
-                break;
-        }
+        add(optionsView, BorderLayout.CENTER);
+        add(buttonPanel, BorderLayout.SOUTH);
     }
 
     public String orderFromRestaurant (){
@@ -70,5 +55,56 @@ public class CustomerOrderView {
         String choice = scanner.nextLine();
         System.out.println("You will order from: " + choice);
         return choice;
+    }
+
+    private void switchView(int choice, Customer customer){
+        setVisible(false);
+        switch(choice) {
+            case 0:
+                //TODO: use RestaurantListView here
+                customerController.viewRestaurantList();
+                String restaurantName = orderFromRestaurant();
+                customerController.placeOrder(restaurantName, customer);
+                break;
+
+            case 1:
+                customerController.customerOrderHistory(customer);
+                break;
+
+            case 2:
+                //TODO: make a new view for this
+                System.out.println("Updating your account... ");
+                System.out.println("Enter your first name: ");
+                //String newFirstName = scanner.next();
+
+                System.out.println("Enter your last name: ");
+               // String newLastName = scanner.next();
+
+                System.out.println("Enter your address: ");
+                //String newAddress = scanner.next();
+
+                //customerController.updateCustomerAccount(newFirstName, newLastName, newAddress);
+                break;
+
+            case 3:
+                System.out.println("Viewing your current orders...");
+                customerController.showRestaurantOrders(customer);
+                break;
+            case 4:
+                System.out.println("Thank you!");
+                break;
+        }
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        if (e.getSource() == btn_select){
+            int index = optionsList.getSelectedIndex();
+            switchView(index, customer);
+        }
+
+        if (e.getSource() == btn_exit){
+            System.exit(0);
+        }
     }
 }
--- src/View/Restaurant/RestaurantListView.java
+++ src/View/Restaurant/RestaurantListView.java
@@ -1,17 +1,86 @@
 package View.Restaurant;
 
+import Controller.Restaurant.RestaurantController;
+import Controller.Restaurant.RestaurantListController;
+import Model.Actors.Customer;
 import Model.Actors.SysAdmin;
+import Model.Restaurant.Restaurant;
 
-public class RestaurantListView {
-   /**
-    * shows view of restaurants based on role of requesting user
-    */
+import javax.swing.*;
+import javax.swing.table.DefaultTableModel;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.ArrayList;
+import java.util.Vector;
+
+public class RestaurantListView extends JFrame implements ActionListener {
+   private JPanel optionsView = new JPanel();
+   private JButton btn_select, btn_exit;
+
+   private JTable restaurantTable;
+   private DefaultTableModel restaurantTableModel;
+   private Customer customer;
+   private RestaurantListController restaurantListController = new RestaurantListController();
+
+   public static void main(String[] args) {
+      RestaurantListView restaurantListView = new RestaurantListView();
+   }
+   public RestaurantListView(){
+      initCompts();
+   }
+
+   public void initCompts(){
+      setTitle("Restaurant List");
+      setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+      setSize(250, 250);
+      setLocationRelativeTo(null);
+
+      restaurantTableModel = new DefaultTableModel();
+      restaurantTableModel.addColumn("Restaurant Index");
+      restaurantTableModel.addColumn("Restaurant Name");
+      restaurantTable = new JTable(restaurantTableModel);
+      JScrollPane restScrollPane = new JScrollPane(restaurantTable);
+      ArrayList<Restaurant> restList = restaurantListController.viewRestaurantList();
+      System.out.println(restList.get(0).getRestaurantName());
+      for (int i = 0; i < restList.size(); i++){
+         Vector row = new Vector();
+         row.add(i);
+         row.add(restList.get(i).getRestaurantName());
+         restaurantTableModel.addRow(row);
+      }
+
+      JPanel buttonPanel = new JPanel();
+      btn_select = new JButton("Select");
+      btn_select.addActionListener(this);
+      btn_exit = new JButton("Exit");
+      buttonPanel.add(btn_select);
+      buttonPanel.add(btn_exit);
+
+      add(optionsView, BorderLayout.CENTER);
+      add(buttonPanel, BorderLayout.SOUTH);
+      add(restScrollPane, BorderLayout.NORTH);
+
+      setVisible(true);
+   }
+
    public void sysAdminView(SysAdmin sysAdmin){
        //sysAdmin sees all restaurants registered in the system
    }
 
    public void sysAdminViewPending (SysAdmin sysAdmin){
        //sysAdmin sees all restaurants wanting to be added to system
+   }
+
+   @Override
+   public void actionPerformed(ActionEvent e) {
+      if (e.getSource() == btn_select){
+         //get restaurant selected
+         //then switch views
+      }
+      if (e.getSource() == btn_exit){
+         System.exit(0);
+      }
    }
 }
 
