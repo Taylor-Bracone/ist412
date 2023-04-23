1bcf27980b8dcd3de581d4a89756c5d5103da260
curiocheshire <sewerattr@gmail.com>
2023-04-22 23:22:29 -0400

    sys admin text file and making sysadmin ID

--- src/Model/Actors/SysAdmin.java
+++ src/Model/Actors/SysAdmin.java
@@ -4,6 +4,7 @@ import Model.Restaurant.Restaurant;
 
 public class SysAdmin extends User {
 
+    private int adminID;
     /**
      * Used to establish a Actors.Person object
      * Adds, removes, updates restaurant list
@@ -16,5 +17,8 @@ public class SysAdmin extends User {
         super(firstName, lastName, address, phoneNumber);
     }
 
-
+    public SysAdmin(String firstName, String lastName, String address, String phoneNumber, int adminID) {
+        super(firstName, lastName, address, phoneNumber);
+        this.adminID = adminID;
+    }
 }
